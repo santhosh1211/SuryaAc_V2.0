b41e9fe927f5822be90db581485365b2f71b2c96
santhosh1211 <santhosh10dm43@gmail.com>
2019-03-19 17:09:32 +0530

    login activity v1.0

---
+++ app/src/main/java/in/co/santhoshiot/suryaacc_v20/Login_Form.java
@@ -0,0 +1,13 @@
+package in.co.santhoshiot.suryaacc_v20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Login_Form extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login__form);
+    }
+}
