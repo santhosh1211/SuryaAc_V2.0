8bebef51840d53e3f2bbefb4fbf37b0504ded1a7
santhosh1211 <santhosh10dm43@gmail.com>
2019-03-21 15:33:18 +0530

    second activity demo

--- app/src/main/java/in/co/santhoshiot/suryaacc_v20/Login_Form.java
+++ app/src/main/java/in/co/santhoshiot/suryaacc_v20/Login_Form.java
@@ -1,13 +1,104 @@
 package in.co.santhoshiot.suryaacc_v20;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login_Form extends AppCompatActivity {
+
+    private EditText username;
+    private EditText password;
+    private Button Login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login__form);
+
+        username = (EditText)findViewById(R.id.ed_username);
+        password = (EditText)findViewById(R.id.ed_password);
+        Login = (Button)findViewById(R.id.btn_login);
+
+/*
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Login_Form.this, SecondActivity.class);
+                startActivity(intent);
+                validate(username.getText().toString(),password.getText().toString());
+            }
+        });
+ */
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(username.getText().toString(),password.getText().toString());
+            }
+        });
+
+
     }
+
+    private void validate(String username, String password){
+        if((username.equals("user")) && (password.equals("user2"))){
+            Intent intent = new Intent(Login_Form.this, SecondActivity.class);
+            startActivity(intent);
+        }
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
